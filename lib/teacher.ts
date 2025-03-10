4aabd7dc33ca26882eca29c48ff11088b161d1af
fred011 <fmmphahle01@gmail.com>
2025-03-10 08:48:31 +0200

    isTeacher fixed

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,3 +1,7 @@
 export const isTeacher = (userId?: string | null) => {
-  return userId === process.env.NEXT_PUBLIC_TEACHER_ID;
+  if (!userId) {
+    return false;
+  }
+  const teacherIds = process.env.NEXT_PUBLIC_TEACHER_ID?.split(",") || [];
+  return teacherIds.includes(userId);
 };
